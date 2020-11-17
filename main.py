28479297f654b7a472703610a07a9d1faa57a568
ridvancanayakin <42276758+ridvancanayakin@users.noreply.github.com>
2020-11-17 16:16:54 +0300

    Numeric Root Finding Hw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from __future__ import division
 import math
+
 X= input("Hangi şıkkın cevabını istiyorsunuz?")
 if X=="a":
     def f(x):
@@ -21,6 +23,7 @@ elif X=="d":
         return x-1-0.3*math.cos(x)
     def df(x):
         return (3*math.sin(x)/10)+1
+
 #Bisection Method
 xp = 100
 xn = -100
@@ -64,3 +67,36 @@ while abs(f(x_c_1))>10**(-5):
 print("x ",x_c_1)
 print("y ",f(x_c_1))
 print("Secant Methodu ile çözüm ",iter_c," iterasyonda bulundu.")
+#Muller Method
+y_d_2=1
+x_d_0 = -2
+x_d_1 = 1
+x_d_2 = 2
+iter_d = 0
+while abs(y_d_2)>10**(-5):
+    iter_d=iter_d+1
+    y_d_0 = f(x_d_0)
+    y_d_1 = f(x_d_1)
+    y_d_2 = f(x_d_2)
+    d0=y_d_1 - y_d_0
+    d1 = y_d_2 - y_d_1
+    h0= x_d_1 - x_d_0
+    h1 = x_d_2 - x_d_1
+    a0=d0/h0
+    a1=d1/h1
+    a=(a1-a0)/(h1+h0)
+    b=a*h1+a1
+    c=y_d_2
+    x1 = abs(b+math.sqrt(abs(b*b-4*a*c)))
+    x2 = abs(b-math.sqrt(abs(b*b-4*a*c)))
+    if x1 >= x2:
+        x_d_3 = x_d_2 - 2 * c / (b + math.sqrt(abs(b * b - 4 * a * c)))
+    else:
+        x_d_3 = x_d_2 - 2 * c / (b - math.sqrt(abs(b * b - 4 * a * c)))
+    x_d_0=x_d_1
+    x_d_1=x_d_2
+    x_d_2=x_d_3
+    y_d_2 = f(x_d_2)
+print("x ",x_d_2)
+print("y ",f(x_d_2))
+print("Muller Methodu ile çözüm ",iter_d," iterasyonda bulundu.")
